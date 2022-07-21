e1079fa90162ac2265ea4cccc3f3f2d47a4327d5
Jesus <gallardomoralesjesus@gmail.com>
2022-07-21 23:57:03 +0200

    feat: add route for Movie

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
+import Movie from './components/Movie'
 import HomePage from './pages/HomePage'
 
 function App () {
@@ -11,6 +12,7 @@ function App () {
 
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/movie/:id' element={<Movie />} />
 
       </Routes>
     </div>
